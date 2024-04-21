1f0da27f3c291c6ed9717b67d484c78664e8a985
Andrew Raffensperger <raffy@me.com>
2024-04-21 02:45:20 -0700

    fix example

--- src/Contract.sol
+++ src/Contract.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.23;
 import {Types} from "@eth-optimism/contracts-bedrock/src/libraries/Types.sol";
 import {Hashing} from "@eth-optimism/contracts-bedrock/src/libraries/Hashing.sol";
 
-contract A {
+contract Contract {
 	function f() external view {
-		Types.OutputProposal memory p;
+		Types.OutputRootProof memory p;
 		Hashing.hashOutputRootProof(p);
 	}
 }
 
 // Compiler run failed:
-// Error (9553): Invalid type for argument in function call. Invalid implicit conversion from struct Types.OutputProposal memory to struct Types.OutputRootProof memory requested.
+// Error (9553): Invalid type for argument in function call. Invalid implicit conversion from struct Types.OutputRootProof memory to struct Types.OutputRootProof memory requested.
